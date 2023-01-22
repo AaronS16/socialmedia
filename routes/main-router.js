f8ee821697eac03120a8b1d903ecbb67f108b35e
Aaron <soteloaaron16@gmail.com>
2023-01-22 09:49:23 -0600

    Finished Social Media Site

---
+++ routes/main-router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/auth-controller');
+const homeController = require('../controllers/home-controller');
+const postsController = require('../controllers/posts-controller');
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
+
+//Main routes
+
+router.get('/', homeController.getIndex);
+
+router.get('/profile', ensureAuth, postsController.getProfile);
+
+router.get('/feed', ensureAuth, postsController.getFeed);
+
+router.get('/login', authController.getLogin);
+
+router.post('/login', authController.postLogin);
+
+router.get('/logout', authController.logout);
+
+router.get('/signup', authController.getSignup);
+
+router.post('/signup', authController.postSignup);
+
+module.exports = router
